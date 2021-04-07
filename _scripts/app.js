d6f17ea58ae836738a53219f510511a42d7b8f4d
Thanikul Sriuthis <moremeng@dv4.biz>
2021-04-07 14:43:05 +0700

    mod not step box

--- _scripts/app.js
+++ _scripts/app.js
@@ -6,7 +6,7 @@ const afterPrint = () => {
   console.log('Functionality to run after printing');
   let bot = document.getElementById("bot");
   bot.value++;
-  console.log(bot.value);
+  // console.log(bot.value);
 
   if (bot.value > 40 && ((bot.value % 40) == 1)) {
     let box = document.getElementById("box").value++;
@@ -21,38 +21,44 @@ const generate = (print) => {
   let encoder = new Code128Generator();
 
   let lot = document.getElementById("lot").value;
-  let exp = document.getElementById("exp").value;
+  let lotBar = encoder.encode(lot);
+
   let sn = document.getElementById("sn").value;
+  let snBar = encoder.encode(sn);
+
+  let exp = document.getElementById("exp").value;
+  let expBar = encoder.encode(exp)
+
   let box = document.getElementById("box").value;
   let bot = document.getElementById("bot").value;
 
-  document.getElementById("pntLot").innerHTML = `${encoder.encode(lot)}`;
+  let botMod = bot % 40;
+
+  document.getElementById("pntLot").innerHTML = `${lotBar}`;
   document.getElementById("pntLotTxt").innerHTML = `${lot}`;
-  document.getElementById("pntSn").innerHTML = `${encoder.encode(sn)}`;
+  document.getElementById("pntSn").innerHTML = `${snBar}`;
   document.getElementById("pntSnTxt").innerHTML = `${sn}`;
-  document.getElementById("pntExp").innerHTML = `${encoder.encode(exp)}`;
+  document.getElementById("pntExp").innerHTML = `${expBar}`;
   document.getElementById("pntExpTxt").innerHTML = `${exp}`;
   document.getElementById("pntBox").innerHTML = `${box}`;
   document.getElementById("pntBot").innerHTML = `${bot}`;
 
-  // document.getElementById("pntLot2").innerHTML = `${encoder.encode(lot)}`;
-  // document.getElementById("pntExp2").innerHTML = `${encoder.encode(exp)}`;
-  // document.getElementById("pntSn2").innerHTML = `${encoder.encode(sn)}`;
-  // document.getElementById("pntBox2").innerHTML = `${box}`;
-  // document.getElementById("pntBot2").innerHTML = `${bot}`;
+  console.log(`bot = ${bot} Mod = ${botMod}`);
 
+  let clone = document.getElementById('clone');
+  let html = '';
 
+  clone.innerHTML = '';
 
-  // const listGroup = document.querySelector('.list-group');
-  // let cln = listGroup.cloneNode(true);
-  // listGroup.replaceChild(cln, listGroup.childNodes[0]);
+  if (botMod == 1) {
 
-  // var item = document.getElementById("myList").childNodes[0];
+    for (let i = ++bot; i < (bot+39) ; i++) {
 
-  // document.getElementById("print").append(cln);
-  // listGroup.after(cln);
+      html += `<div class="pt-3 mb-0 print-page-break"><div class="row"><div class="col-3 pt-3">Lot No.</div><div class="col-8"><span class="barcode d-block">${lotBar}</span><span class="d-block text-barcode">${lot}</span></div></div><div class="row"><div class="col-3 pt-3">S/N</div><div class="col-8"><span class="barcode d-block">${snBar}</span><span class="d-block text-barcode">${sn}</span></div></div><div class="row"><div class="col-3 pt-3">Exp</div><div class="col-8"><span class="barcode d-block">${expBar}</span><span class="d-block text-barcode">${exp}</span></div></div><div class="row"><div class="col-3"></div><div class="col-8 row"><div class="col">กล่องที่ <span>${box}</span></div><div class="col">ขวดที่ <span>${i}</span></div></div></div></div>`;
+    }
+    clone.innerHTML = html;
+  }
 
-  // listGroup.removeChild(cln);
   if (print)
     window.print();
 
